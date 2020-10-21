b2875d8d7fc97b1e64e50e2fd069f8a0897a50d5
OzdGini <ozd@gini-apps.com>
2020-10-21 12:19:07 +0300

    Initial Commit

---
+++ CLDVideoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CLDVideoView
+//
+//  Created by Oz Deutsch on 21/10/2020.
+//  Copyright © 2020 Cloudinary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
