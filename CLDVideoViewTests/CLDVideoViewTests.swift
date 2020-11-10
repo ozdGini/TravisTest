5a999f42514998f9db35acec05031f834be75c5e
OzdGini <ozd@gini-apps.com>
2020-11-10 16:34:29 +0200

    changed travis yml

---
+++ CLDVideoViewTests/CLDVideoViewTests.swift
@@ -0,0 +1,33 @@
+//
+//  CLDVideoViewTests.swift
+//  CLDVideoViewTests
+//
+//  Created by Oz Deutsch on 04/11/2020.
+//  Copyright © 2020 Cloudinary. All rights reserved.
+//
+
+import XCTest
+
+class CLDVideoViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
